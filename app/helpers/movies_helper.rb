371aabb262bbb899ccc6c2ad877a8f1367522352
fullerenedream <fullerenedream@gmail.com>
2016-03-30 02:40:25 +0000

    step 16 done

--- app/helpers/movies_helper.rb
+++ app/helpers/movies_helper.rb
@@ -1,2 +1,11 @@
 module MoviesHelper
+
+  def formatted_date(date)
+    # if !date.nil?
+      date.strftime("%b %d, %Y")
+    # else
+    #   "Release date not yet provided."
+    # end
+  end
+  
 end
